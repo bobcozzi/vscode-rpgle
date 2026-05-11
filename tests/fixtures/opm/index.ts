7b68d1d279b65af573ba7c5c3e8c88bf1fd2ceb3
Mohammed-Yaseen-Ali1 <Mohammed.Yaseen.Ali1@ibm.com>
2026-05-11 14:15:52 +0530

    Remove User Test fixtures | Sanitize and rename Files, variables etc

--- tests/fixtures/opm/index.ts
+++
@@ -1,8 +0,0 @@
-
-import { readFile } from "fs/promises";
-import path from "path";
-
-export function readFixture(fileName: string): Promise<string> {
-  const fixturePath = path.join(__dirname, fileName);
-  return readFile(fixturePath, "utf-8");
-}
